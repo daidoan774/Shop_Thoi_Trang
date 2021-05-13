3e1fdc11a26db5fcf0a5a3b0aa8aa3444ce5d8a1
Tan Dai <daidoan774@gmail.com>
2021-05-13 22:58:25 +0700

    update_website_v01

---
+++ Shop_Thoi_Trang2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Shop_Thoi_Trang2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop_Thoi_Trang2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var message = new MessageModel1();
+            message.Welcome = "Hello come to Shop";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
